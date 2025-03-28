23854092c628bbd4fe8c6c936b923e2d70f2a764
trandung09 <trandung09082004@gmail.com>
2025-03-28 16:54:05 +0700

    initial commit

---
+++ src/main/java/com/tvd/globetrekplatform/api/GlobetrekPlatformApiApplication.java
@@ -0,0 +1,13 @@
+package com.tvd.globetrekplatform.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobetrekPlatformApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobetrekPlatformApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tvd/globetrekplatform/api/GlobetrekPlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvd.globetrekplatform.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobetrekPlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
